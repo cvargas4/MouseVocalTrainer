0e95a74a732d910b20dc907952472d6f08032876
César Vargas <33707956+cvargas4@users.noreply.github.com>
2021-09-20 14:56:30 -0400

    Delete Modified FED3 Code/FED3_wClosedLoop.ino directory

--- Modified FED3 Code/FED3_wClosedLoop.ino/OptoStim.ino
+++
@@ -1,8 +0,0 @@
-void optoStim(){
-//  Blink(BNC_OUT, 25, 20);  // Set stimulation parameters, PIN/Pulse_Length/Pulse_number
-    Feed();
-    CheckForPellet();
-    logdata();
-    digitalWrite(BNC_OUT,LOW);
-    UpdateDisplay();
-}
